461c193133725abd2d2ccab214c02212cbace0be
reuben <r_fiet@yahoo.com>
2015-09-15 12:59:40 -0700

    status
    Signed-off-by: reuben <r_fiet@yahoo.com>

---
+++ script.js
@@ -0,0 +1,155 @@
+/*
+  Reuben Fiet
+  Assignment 1
+  Front End Dev 2
+  9/15/2015
+*/
+
+//Step 1
+//var someMonth;
+//function theMonth; 		                 // function to return current month
+//const currentMonth; 		                 // a constant
+//var summerMonth; 		                     // an array of summer months
+//function myLibraryAFunction;	             // a function 
+
+//Step 2
+//var numLiteral = 5.4;
+//var strLiteral = "Assignment 1";
+//var boolLiteral = false;
+//var x = null;
+
+//Step 3
+//var y = 5 + (4 * 2)/1.5;
+//var z = y + Math.PI;
+
+// Step 4
+//var strFirstName, strLastName, strAddress, strCity, strState, intZipCode, intYourAge, strReferralSource, boolMayWeContactYou;
+
+//Step 5
+//var strFirstName = "Reuben";
+//var strLastName;
+//strLastName = "Fiet";
+//var strCity = "San Diego", strState = "CA";
+
+//Step 6
+//var numStr =  7 + " Hello";
+//console.log("numStr = " + numStr);
+//var testBool = false;
+//var boolStr = testBool + " Help Me";
+//console.log("boolStr = " + boolStr);
+//var testBool2 = true;
+//var boolNum = testBool2 + 8;
+//console.log("boolNum = " + boolNum);
+
+//Step 7
+//console.log("my var = " + hoistVar);
+//var hoistVar = "I shoudl be undefined";
+
+// Step 8
+// orginal string was not valid. 
+// need to escape the apostrophe
+//var someString = 'Who once said, "Only two things are infinite, the universe and human stupidity, and I\'m not sure about the former."';
+//console.log(someString);
+
+// Step 9
+//var step9;
+//var step9Null = null;
+//console.log(step9Null);
+
+//Step 10
+//var strX = "My String";
+//console.log(typeof(strX));
+//var numX = 11;
+//console.log(typeof(numX));
+//var boolX = false;
+//console.log(typeof(boolX));
+//var objX = new Object();
+//console.log(typeof(objX));
+//var undefX;
+//console.log(typeof(undefX));
+
+// Step 11
+//alert("Hello " + "Zak Ruvalcaba" + ", welcome to the JavaScript class!");
+
+//Step 12
+//var name = "Reuben Fiet";
+//alert("Hello " + name + ", welcome to the JavaScript class!");
+ 
+// Step 13
+//var course = "Javascript";
+//var name = "Reuben Fiet";
+//var mainStr = "Hello " + name + ", welcome to the " + course + " class!";
+//alert(mainStr);
+
+// Step 14
+//var course = "Javascript";
+//var name = "Reuben Fiet";
+//var mainStr = "Hello " + name + ", \nwelcome to the " + course + " class!";
+//alert(mainStr);
+
+
+//Step 15
+//var course = "Javascript";
+//var name = prompt("Please enter your name");
+//var mainStr = "Hello " + name + ", \nwelcome to the " + course + " class!";
+//alert(mainStr);
+
+
+//Step 16
+//var course = prompt("What class are you taking?");
+//var name = "Reuben Fiet"; 
+//var mainStr = "Hello " + name + ", \nwelcome to the " + course + " class!";
+//alert(mainStr);
+
+//Step 17
+//var x = 10;
+//var y = 20;
+//console.log( x + y);
+
+//Step 18
+//var x = 20;
+//x += 20;
+//console.log( x );
+
+//Step 19
+//var x = 20;
+//x *= 5;
+//console.log( x );
+
+//Step 20
+//var x = 20 % 3;
+//x /= 1;
+//console.log( x );
+
+//Step 21
+//var x = 20;
+//var y = 5;
+//var z = true;
+//if(x > y && z){
+//   console.log( x + " > " + y  );
+//}
+
+//Step 22
+//var urlStr = "yahoo.com";
+
+//if (!(urlStr.length > 2 && urlStr.indexOf("google") > -1) ) {
+//    console.log( "The url String " + urlStr + " is not Google");   
+//}
+
+//Step 23
+//var widget = new Object();
+ //console.log( "widget = " + typeof(widget) );
+
+//Step 24
+//var widget = new Object();
+//console.log(widget instanceof Object);
+
+//Step 25
+//var widget = new Object();
+//if(!(widget  instanceof Object)){
+//    console.log(w"widget is instanceof Object");
+//}
+//else{
+    
+//  console.log("widget is not instanceof Object");  
+//}
